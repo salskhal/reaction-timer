62f9607051fbc5d2f683b3a92ca11dc6730444db
salskhal <olaniyisal@gmail.com>
2022-03-13 22:10:56 +0100

    Initialization

---
+++ src/components/Block.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="block" v-if="showBlock" @click="stopTimer">click me</div>
+</template>
+
+<script>
+export default {
+  props: ["delay"],
+  data() {
+    return {
+      showBlock: false,
+      timer: null,
+      reactionTime: 0
+    };
+  },
+  mounted() {
+    setTimeout(() => {
+      this.showBlock = true;
+      console.log(this.delay);
+      this.startTimer()
+    }, this.delay);
+  },
+  methods: {
+    startTimer() {
+        setInterval(() => {
+            this.reactionTime += 10
+        }, 10)
+    },
+    stopTimer() {
+        clearInterval(this.timer)
+        console.log(this.reactionTime)
+        this.$emit("end", this.reactionTime)
+    },
+  },
+};
+</script>
+
+<style>
+.block {
+  width: 400px;
+  border-radius: 20px;
+  background: #0faf8f;
+  color: white;
+  text-align: center;
+  padding: 100px 0;
+  margin: 40px auto;
+}
+</style>
---
+++ src/components/Result.vue
@@ -0,0 +1,32 @@
+<template>
+  <h1>Your reaction time is {{ score }}</h1>
+  <p class="rank">{{rank}}</p>
+</template>
+
+<script>
+export default {
+    props: [ "score" ],
+    data(){
+        return{
+            rank: null
+        }
+    },
+    mounted(){
+        if(this.score < 250){
+            this.rank = "Ninga Finger"
+        } else if(this.score >= 250 && this.score <= 650){
+            this.rank = "Fast reflexes"
+        } else{
+            this.rank = "Bet a snail is faster than you are"
+        }
+    }
+}
+</script>
+
+<style>
+.rank{
+    font-size: 1.4rem;
+    color: #0faf87;
+    font-weight: bold;
+}
+</style>
